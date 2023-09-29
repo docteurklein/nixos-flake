cbf872a3df8b36607081b413f7018e1a828b2191
Florian Klein <florian.klein@free.fr>
2023-09-29 09:46:31 +0200

    grafana + prometheus to monitor self

--- nixosModules/common.nix
+++ nixosModules/common.nix
@@ -53,6 +53,34 @@
   };
 
   services = {
+    grafana = {
+      enable = true;
+      settings = {
+        server = {
+          http_addr = "127.0.0.1";
+          http_port = 3000;
+          domain = "localhost";
+        };
+      };
+    };
+    prometheus = {
+      enable = true;
+      port = 9001;
+      exporters = {
+        node = {
+          enable = true;
+          port = 9002;
+        };
+      };
+      scrapeConfigs = [
+        {
+          job_name = "nixos";
+          static_configs = [{
+            targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+          }];
+        }
+      ];
+    };
     logrotate = {
       enable = true;
     };
